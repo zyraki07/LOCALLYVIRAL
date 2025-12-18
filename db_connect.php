028a0ecbc4ca89d47b11ca479725f6235f581ad7
zyraki07 <aisforr0@gmail.com>
2025-12-18 15:57:03 +0800

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "businesses";
+$servername = getenv("MYSQLHOST");
+$username = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$dbname = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, (int)$port);
 
 if(!$conn) {
     die ("connection failed: " . mysqli_connect_error());
